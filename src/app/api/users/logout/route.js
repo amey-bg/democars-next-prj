6f24879bc2c755cd56b038598c8351be7133df1b
Amey Bhagwat <amey.bg@gmail.com>
2024-01-05 23:03:02 +0530

    changed logout api again

--- src/app/api/users/logout/route.js
+++ src/app/api/users/logout/route.js
@@ -1,16 +1,25 @@
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
-export async function GET() {
+export async function GET(request) {
   try {
+    const cookieStore = cookies();
+    const token1 = cookieStore.get("token");
     const response = NextResponse.json(
       { data: null, message: "User logged out successfully!" },
-      { status: 200 }
+      {
+        status: 200,
+        headers: {
+          "content-type": "application/json",
+          "Set-Cookie": `token=${token1.value}; Path=/; Max-Age=0`,
+        },
+      }
     );
 
     console.log("Logout API response =", response);
 
     //Clear / remove cookie
-    response.cookies.set("token", "", { expires: new Date(Date.now()) });
+    response.cookies.set("token", null, { expires: new Date(Date.now()) });
     response.cookies.delete("token");
 
     return response;
